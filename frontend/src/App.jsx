0e450dc5568bf271c8dbe9f874050d0f8ab806e5
mini-360 <rajabhay56999@gmail.com>
2024-09-24 00:24:24 +0530

    navbar route setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
-import Navbar from "./components/navbar.component.jsx"
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar.component.jsx";
 
-export default function App() {
+const App = () => {
   return (
-    <Navbar/>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route path="signin" element={<h1>Sign in page</h1>} />
+        <Route path="signup" element={<h1>Sign up page</h1>} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
