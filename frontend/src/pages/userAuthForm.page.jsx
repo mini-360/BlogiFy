2d15be7fee79079a97847d470538bde6a6e5e1ed
mini-360 <rajabhay56999@gmail.com>
2024-09-25 23:45:55 +0530

    ui connected to server

--- frontend/src/pages/userAuthForm.page.jsx
+++ frontend/src/pages/userAuthForm.page.jsx
@@ -1,13 +1,75 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 import InputBox from "../components/input.component.jsx";
 import googleIcon from "../imgs/google.png";
 import AnimationWrapper from "../common/page-animation.jsx";
+import { Toaster, toast } from "react-hot-toast";
+import axios from "axios";
+
+
+
+
+import { storeInSession } from "../common/session.jsx";
 
 const UserAuthForm = ({ type }) => {
+ 
+  const userAuthThroughServer = (serverRoute, formData) => {
+    axios
+      .post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, formData)
+      .then(({ data }) => {
+        storeInSession("user", JSON.stringify(data))
+        console.log(sessionStorage);
+        
+      })
+      .catch(({ response }) => {
+        toast.error(response.data.error);
+      });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let serverRoute =
+      type == "sign-in" ? "/api/users/signin" : "/api/users/signup";
+
+    let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+
+    // formData
+    let form = new FormData(formElement);
+    let formData = {};
+
+    for (let [key, value] of form.entries()) {
+      formData[key] = value;
+    }
+
+    let { fullname, email, password } = formData;
+    // form validation
+
+    if (fullname) {
+      if (fullname.length < 3) {
+        return toast.error("Fullname must be at least 3 letters long");
+      }
+    }
+    if (!email.length) {
+      return toast.error("Enter email");
+    }
+    if (!emailRegex.test(email)) {
+      return toast.error("Email is Invalid");
+    }
+    if (!passwordRegex.test(password)) {
+      return toast.error(
+        "Password should be 6 to 20 characters long with a numeric, 1 lowercase and 1 uppercase letters"
+      );
+    }
+
+    userAuthThroughServer(serverRoute, formData);
+  };
   return (
     <AnimationWrapper keyValue={type}>
       <section className="h-cover flex items-center justify-center">
-        <form className="w-[80%] max-w-[400px]">
+        <Toaster />
+        <form id="formElement" className="w-[80%] max-w-[400px]">
           <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
             {type == "sign-in" ? "Welcome back" : "Join us today"}
           </h1>
@@ -33,7 +95,11 @@ const UserAuthForm = ({ type }) => {
             placeholder="Password"
             icon="fi-rr-key"
           />
-          <button className="btn-dark center mt-14" type="submit">
+          <button
+            className="btn-dark center mt-14"
+            type="submit"
+            onClick={handleSubmit}
+          >
             {type.replace("-", " ")}
           </button>
           <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
